1e4021cc624c8584ef24455729c2eb4ac9080ab3
koheron <koheron2@gmail.com>
2017-02-18 17:47:32 +0000

    initial commit

--- create_box/main.py
+++ create_box/main.py
@@ -2,7 +2,14 @@
 
 
 def create_box(height, width, character):
-    pass
+    box = ''
+    for i in range(height):
+        box_width = ''
+        for j in range(width):
+            box_width += character
+        box_width += '\n'
+        box += box_width
+    return box
 
 
 if __name__ == '__main__':
