0b351b6e49b5b60ed2206c12e776de59b0f75c46
koheron <koheron2@gmail.com>
2017-02-18 21:49:20 +0000

    Third Commit (trimmed plus while version)

--- create_box/main.py
+++ create_box/main.py
@@ -1,6 +1,6 @@
 """This is the entry point of the program."""
 
-def create_box(height, width, character):
+"""def create_box(height, width, character):
     box = ''
     for i in range(height):
         box_width = ''
@@ -8,7 +8,31 @@ def create_box(height, width, character):
             box_width += character
         box_width += '\n'
         box += box_width
+    return box"""
+    
+    #shortened version
+def create_box(height, width, character):
+    box = ''
+    for i in range(height):
+        for j in range(width):
+            box += character
+        box += '\n'
     return box
+    
+    """while version
+def create_box(height, width, character):
+    box = ''
+    count_height = 0
+    count_width = 0
+    while count_width < width and count_height < height:
+        box += character
+        count_width += 1
+        if count_width == width:
+            box += '\n'
+            count_width = 0
+            count_height += 1
+    return box"""
+
     
     
 def create_special_box(height, width, character):
