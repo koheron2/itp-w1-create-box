aeed1e13bfca746dedddc1ee522a6171798f686e
koheron <koheron2@gmail.com>
2017-02-20 20:58:09 +0000

    create_special_box modified to utilize looping.

--- create_box/main.py
+++ create_box/main.py
@@ -10,12 +10,26 @@
         box += box_width
     return box"""
     
-    #shortened version
+    #shortened version (commented code is what is added for to create_special_box)
 def create_box(height, width, character):
     box = ''
     for i in range(height):
         for j in range(width):
+                #if i == 0 or i == height - 1 or j == 0 or j == width - 1:
             box += character
+            #else:
+                #box += ' '
+        box += '\n'
+    return box
+    
+def create_special_box(height, width, character):
+    box = ''
+    for i in range(height):
+        for j in range(width):
+            if i == 0 or i == height - 1 or j == 0 or j == width - 1:
+                box += character
+            else:
+                box += ' '
         box += '\n'
     return box
     
@@ -35,13 +49,28 @@ def create_box(height, width, character):
 
     
     
-def create_special_box(height, width, character):
+"""def create_special_box(height, width, character):
     topbot = width*character + '\n'
     middle_line = character + ((width-2) * " ") + character + '\n'
     middle = middle_line * (height - 2)
     final_box = topbot + middle + topbot
     
-    return final_box
+    return final_box"""
+    
+"""Another create_special_box function, by Viet Pham
+def create_special_box(height, width, character):
+    topbot = width*character + '\n'
+    middle = ''
+    for i in range(height-2):
+ +        box_width = character
+ +        for j in range(width-2):
+ +            box_width += ' '
+ +        box_width += character + '\n'
+ +        middle += box_width
+ +    
+ +    finalbox = topbot + middle + topbot
+ +    
+ +    return finalbox"""
 
 
 if __name__ == '__main__':
